af257ef2fea6fad9d85aec9b15c9adc3d6b1775b
ganesha <ganesha@localhost>
2015-06-10 16:54:37 +0200

    quartier par flo

---
+++ LyonY/src/fr/cursusSopra/model/Quartier.java
@@ -0,0 +1,16 @@
+package fr.cursusSopra.model;
+
+public class Quartier {
+
+	private int idquartier;
+	private String nom;
+	private int budgetMoyen;
+	private int distanceCentreVille;
+	
+	
+	
+	
+	
+	
+	
+}
