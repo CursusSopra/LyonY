7dafbf296934264846ab4227562a250dfba46a3b
gjouen <gaidig.jouen@gmail.com>
2015-06-11 14:06:47 +0200

    Code semi-fonctionnel actions bar

---
+++ LyonY/src/fr/cursusSopra/action/ListeDesBarsAction.java
@@ -0,0 +1,47 @@
+package fr.cursusSopra.action;
+
+import java.sql.SQLException;
+
+import java.util.List;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+import fr.cursusSopra.model.Bar;
+
+public class ListeDesBarsAction extends ActionSupport {
+
+	private static final long serialVersionUID = 1L;
+
+	public List<Bar> listeDesBars;
+
+	public List<Bar> getListeDesBars() {
+
+		return listeDesBars;
+
+	}
+
+	public ListeDesBarsAction() {
+
+	}
+
+	public String execute() {
+
+		try {
+
+			listeDesBars = Bar.getListeDesBars();
+
+			return SUCCESS;
+
+		} catch (SQLException e) {
+
+			// TODO Auto-generated catch block
+
+			e.printStackTrace();
+
+			return ERROR;
+
+		}
+
+	}
+
+}
