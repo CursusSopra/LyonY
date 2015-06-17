7ba29baebf6883528db8179c14de4958d983300a
rgesnik <remy.gesnik@orange.fr>
2015-06-17 11:38:00 +0200

    JSON affichage pseudo et mail dispo dans le formulaire de création utilisateur : fonctionel

--- LyonY/WebContent/_js/creationUtilisateur.js
+++ LyonY/WebContent/_js/creationUtilisateur.js
@@ -3,7 +3,6 @@
  */
 $(function() {
 	var selectedPseudo = $('#idNewPseudo').val();
-	var selectedEmail = $('#idNewEmail').val();
 	$('#idNewPseudo')
 		.blur(function() {
 			var $this = $(this);
@@ -19,31 +18,31 @@ $(function() {
 			 			$this.closest('.form-group').addClass("has-error").removeClass("has-success");
 			 			$this.parent().next('span').html('Ce pseudo n&apos;est pas disponible, merci d&apos;en choisir un autre.');
 			 		}
-			 		var szOption = '<option>Pseuso disponible</option>';
-		 		})
+			 	})
 		 		.fail(function(indispo) { /* affiche pseudo indisponible */
 		 		});
 		});
-	
+});
+
+$(function() {
+	var selectedEmail = $('#idNewEmail').val();
 	$('#idNewEmail')
-	.blur(function() {
-		var $this = $(this);
-		var selectedEmail = $('#idNewEmail').val();	
-		$.getJSON('getJSONemail?email=' + selectedEmail)
-		 	.success(function(data) { /* affiche pseudo dispo */
-		 		if(data.emailDispo) {
-			 		console.log(data.emailDispo);
-			 		$this.closest('.form-group').addClass("has-success").removeClass("has-error");
-			 		$this.parent().next('span').html('Le pseudo est libre.');
-			 		
-		 		} else {
-		 			$this.closest('.form-group').addClass("has-error").removeClass("has-success");
-		 			$this.parent().next('span').html('Cette adresse mail n&apos;est pas disponible, merci d&apos;en choisir une autre.');
-		 		}
-		 		var szOption = '<option>Pseuso disponible</option>';
-	 		})
-	 		.fail(function(indispo) { /* affiche pseudo indisponible */
-	 		});
-	});
-	
+		.blur(function() {
+			var $this = $(this);
+			var selectedEmail = $('#idNewEmail').val();	
+			$.getJSON('getJSONemail?email=' + selectedEmail)
+			 	.success(function(data) { /* affiche mail dispo */
+			 		if(data.emailDispo) {
+				 		console.log(data.emailDispo);
+				 		$this.closest('.form-group').addClass("has-success").removeClass("has-error"); /* .closest('.form-group') évite de cumuler pleins de .parent() à la suite*/
+				 		$this.parent().next('span').html('L&apos;adresse mail est libre.');
+				 		
+			 		} else {
+			 			$this.closest('.form-group').addClass("has-error").removeClass("has-success");
+			 			$this.parent().next('span').html('Cette adresse mail n&apos;est pas disponible, merci d&apos;en choisir une autre.');
+			 		}
+			 	})
+		 		.fail(function(indispo) { /* affiche mail indisponible */
+		 		});
+		});
 });
