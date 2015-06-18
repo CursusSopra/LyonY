dc78e2d9ec5d7fcc209b5f19152d1cb59af1b5c2
gjouen <gaidig.jouen@gmail.com>
2015-06-18 16:43:32 +0200

    Modifications fonction tri liste

--- LyonY/WebContent/_js/sorties/listeDesBars.js
+++ LyonY/WebContent/_js/sorties/listeDesBars.js
@@ -2,5 +2,19 @@
  * 
  */
 $(document).ready(function() {
-	$('#listeDesBars').DataTable();
+	$('#listeDesBars').DataTable({
+		   "language": {
+	            "lengthMenu": "Montrer _MENU_ items par page",
+	            "zeroRecords": "Aucun résultat",
+	            "info": "Page _PAGE_ sur _PAGES_",
+	            "infoEmpty": "Aucun résultat",
+	            "infoFiltered": "(filtered from _MAX_ total records)",
+	            "search" : "Rechercher",
+	            "paginate": {        
+	            			"next":       "Suivant",        
+	            			"previous":   "Précédent"
+	            }
+	            
+	        },
+	});
 });
